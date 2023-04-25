3741225e896c20953509acc93cc99b186c259d84
dev-ntq-hienns-at-130944112105 <19021269@vnu.edu.vn>
2023-04-25 21:00:54 +0700

    update

--- controllers/auth.controlller.js
+++ controllers/auth.controlller.js
@@ -5,6 +5,7 @@ const User = require("../models/user.model");
 class AuthController {
   static async login(req, res) {
     try {
+      console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
       if (req.body.credential) {
         const verificationResponse = await verifyGoogleToken(
           req.body.credential
@@ -16,30 +17,18 @@ class AuthController {
           });
         }
 
-        const profile = verificationResponse.payload;
+        const profile = verificationResponse?.payload;
 
         let user = await User.findOne({ email: profile.email });
         if (!user) {
           user = new User({
-            email: profile.email,
-            firstName: profile.given_name,
-            lastName: profile.family_name,
-            picture: profile.picture,
+            email: profile?.email,
+            firstName: profile?.given_name,
+            lastName: profile?.family_name,
+            picture: profile?.picture,
           });
           await user.save();
         }
-        // const profile = verificationResponse?.payload;
-
-        // let user = await User.findOne({ email: profile.email });
-        // if (!user) {
-        //   user = new User({
-        //     email: profile?.email,
-        //     firstName: profile?.given_name,
-        //     lastName: profile?.family_name,
-        //     picture: profile?.picture,
-        //   });
-        //   await user.save();
-        // }
 
         res.status(201).json({
           message: "Login was successful",
